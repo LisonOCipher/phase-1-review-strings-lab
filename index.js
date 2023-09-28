d32ddaa9789ed011668afe40b2f8eda8b6ce3daa
Danson Lino <danson.lino@student.moringaschool.com>
2023-09-29 00:32:36 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Runner Jumpman';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeeting = `Welcome, ${currentUser.slice(0, 1)}!`;
